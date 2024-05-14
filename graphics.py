0c38409734d088b176dc06abd7726f15020fb2c1
DimRev <dimrev444@gmail.com>
2024-05-14 09:40:12 +0300

    Create a cell obj

--- graphics.py
+++ graphics.py
@@ -16,10 +16,50 @@ class Line:
         self.__p1 = p1
         self.__p2 = p2
 
-    def draw(self, canvas: Canvas, fill_color):
+    def draw(self, canvas: Canvas, fill_color: str):
         canvas.create_line(
             self.__p1.x, self.__p1.y, self.__p2.x, self.__p2.y, fill=fill_color, width=2
         )
+
+
+class Cell(object):
+    """docstring for Cell."""
+
+    def __init__(
+        self,
+        p1: Point,
+        p2: Point,
+        has_left_wall=True,
+        has_top_wall=True,
+        has_right_wall=True,
+        has_bottom_wall=True,
+    ):
+        self.__x1 = p1.x
+        self.__y1 = p1.y
+        self.__x2 = p2.x
+        self.__y2 = p2.y
+        self.has_left_wall = has_left_wall
+        self.has_top_wall = has_top_wall
+        self.has_right_wall = has_right_wall
+        self.has_bottom_wall = has_bottom_wall
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        p1 = Point(self.__x1, self.__y1)
+        p2 = Point(self.__x2, self.__y1)
+        p3 = Point(self.__x2, self.__y2)
+        p4 = Point(self.__x1, self.__y2)
+        if self.has_top_wall:
+            top_line = Line(p1, p2)
+            top_line.draw(canvas, fill_color)
+        if self.has_right_wall:
+            right_line = Line(p2, p3)
+            right_line.draw(canvas, fill_color)
+        if self.has_bottom_wall:
+            bottom_line = Line(p3, p4)
+            bottom_line.draw(canvas, fill_color)
+        if self.has_bottom_wall:
+            left_line = Line(p4, p1)
+            left_line.draw(canvas, fill_color)
 
 
 class Window:
@@ -46,5 +86,8 @@ class Window:
     def close(self):
         self.__running = False
 
-    def draw_line(self, line: Line):
-        line.draw(self.__canvas, "black")
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw(self.__canvas, fill_color)
+
+    def draw_cell(self, cell: Cell, fill_color: str):
+        cell.draw(self.__canvas, fill_color)
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from graphics import Window, Line, Point
+from graphics import Window, Line, Point, Cell
 
 
 def main():
     win = Window(800, 600)
     p1 = Point(10, 10)
-    p2 = Point(10, 20)
-    p3 = Point(20, 20)
-    p4 = Point(20, 10)
-    l1 = Line(p1, p2)
-    l2 = Line(p2, p3)
-    l3 = Line(p3, p4)
-    l4 = Line(p4, p1)
-    win.draw_line(l1)
-    win.draw_line(l2)
-    win.draw_line(l3)
-    win.draw_line(l4)
+    # p2 = Point(10, 20)
+    p3 = Point(40, 80)
+    # p4 = Point(20, 10)
+    # l1 = Line(p1, p2)
+    # l2 = Line(p2, p3)
+    # l3 = Line(p3, p4)
+    # l4 = Line(p4, p1)
+    # win.draw_line(l1, "black")
+    # win.draw_line(l2, "black")
+    # win.draw_line(l3, "black")
+    # win.draw_line(l4, "black")
+    cell = Cell(p1, p3)
+    win.draw_cell(cell, "black")
     win.wait_for_close()
 
 
